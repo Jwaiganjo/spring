7ded1f160994689636854842236846f920c0dc04
GEOFREY WAIGANJO <95221630+Jwaiganjo@users.noreply.github.com>
2024-05-13 19:40:05 +0300

    Update settings.py

--- subspace/settings.py
+++ subspace/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_r4wffz$^66nn0%b&1(n!+13(67!j=hni2#2j81r#r^p7%vq9b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
